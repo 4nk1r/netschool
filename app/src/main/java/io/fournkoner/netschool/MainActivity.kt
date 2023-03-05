9e816552a8847fe2c326e2f14a44aad0d8812f04
4nk1r <4nk1r.dev@gmail.com>
2023-03-05 14:03:49 +0500

    splash screen

--- app/src/main/java/io/fournkoner/netschool/MainActivity.kt
+++ app/src/main/java/io/fournkoner/netschool/MainActivity.kt
@@ -1,11 +1,13 @@
 package io.fournkoner.netschool
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,6 +18,7 @@ import io.fournkoner.netschool.ui.style.NetSchoolTheme
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             val isDarkTheme = isSystemInDarkTheme()
